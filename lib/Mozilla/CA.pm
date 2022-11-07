b6755f5d74b5cede06277f306d33ebd73fd0e05b
Graham Knop <haarg@haarg.org>
2022-11-07 17:48:06 +0100

    clean up conversion to absolute path

--- lib/Mozilla/CA.pm
+++ lib/Mozilla/CA.pm
@@ -3,16 +3,12 @@ package Mozilla::CA;
 use strict;
 our $VERSION = '20211001';
 
-use Cwd ();
 use File::Spec ();
 use File::Basename qw(dirname);
 
 sub SSL_ca_file {
     my $file = File::Spec->catfile(dirname(__FILE__), "CA", "cacert.pem");
-    if (!File::Spec->file_name_is_absolute($file)) {
-        $file = File::Spec->catfile(Cwd::cwd(), $file);
-    }
-    return $file;
+    return File::Spec->rel2abs($file);
 }
 
 1;
